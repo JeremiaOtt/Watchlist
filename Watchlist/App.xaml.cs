ff0c49336b37f9e9c047179f0c7eb5799cd7eaa8
Seischen <seischen.night@yahoo.de>
2020-05-21 19:52:57 +0200

    Documentation autofac

--- Watchlist/App.xaml.cs
+++ Watchlist/App.xaml.cs
@@ -13,5 +13,34 @@ namespace Watchlist
     /// </summary>
     public partial class App : Application
     {
+        //private const string RootLifetimeTag = "MyIsolatedRoot";
+
+        //private void App_Startup(object sender, StartupEventArgs e)
+        //{
+        //    //var serviceCollection = new ServiceCollection();
+        //    //serviceCollection.AddLogging();
+
+        //    var containerBuilder = new ContainerBuilder();
+        //    //containerBuilder.RegisterType<MessageHandler>().As<IHandler>();
+        //    var container = containerBuilder.Build();
+
+        //    using (var scope = container.BeginLifetimeScope(RootLifetimeTag))
+        //    {
+        //        // This service provider will have access to global singletons
+        //        // and registrations but the "singletons" for things registered
+        //        // in the service collection will be "rooted" under this
+        //        // child scope, unavailable to the rest of the application.
+        //        //
+        //        // Obviously it's not super helpful being in this using block,
+        //        // so likely you'll create the scope at app startup, keep it
+        //        // around during the app lifetime, and dispose of it manually
+        //        // yourself during app shutdown.
+        //        //var serviceProvider = new AutofacServiceProvider(scope);
+        //        var mainWin = new MainWindow();
+        //        mainWin.DataContext = container.Resolve<Manager>();
+        //        mainWin.Show();
+        //    }
+        //}
+        // https://autofaccn.readthedocs.io/en/latest/integration/netcore.html#
     }
 }
