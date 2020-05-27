de1a6664185dd5b8da1905e4d9ba556940474e64
Seischen <seischen.night@yahoo.de>
2020-05-27 12:56:05 +0200

    Add dummy button

--- Watchlist/Pages/EntryList.xaml.cs
+++ Watchlist/Pages/EntryList.xaml.cs
@@ -22,5 +22,10 @@ namespace Watchlist.Pages
         {
             InitializeComponent();
         }
+
+        private void Add_Entry_Click(object sender, RoutedEventArgs e)
+        {
+            //_manager.AddEntry();
+        }
     }
 }
